0ce9a732463b19be0f3f759ec8e058ea3c18d847
Art <77998829+artdlc20973@users.noreply.github.com>
2022-07-11 23:40:00 -0600

    signup form with validation
    simple signup form with validation

---
+++ signupForm/script.js
@@ -0,0 +1,55 @@
+const form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // validation
+  const username = form.username.value;
+
+  if(usernamePattern.test(username)) {
+    // feedback good info
+    feedback.textContent = 'that username is valid!';
+  } else {
+    // feedback help info
+    feedback.textContent = 'username must contain letters only and be between 6 - 12 characters long!'
+  }
+
+});
+
+
+// live feedback
+form.username.addEventListener('keyup', e => {
+  // console.log(e.target.value, form.username.value);
+  if(usernamePattern.test(e.target.value)) {
+    form.username.setAttribute('class', 'success');
+  } else {
+    form.username.setAttribute('class', 'error');
+  }
+});
+
+
+
+
+
+
+
+
+
+// test 1
+// const form = document.querySelector('.signup-form');
+// const username = document.querySelector('#username');
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+// });
+
+// // testing RegEx
+// const username = 'artcodes2';
+// const pattern = /[a-z]{6,}/;
+
+// let result = pattern.test(username);
+// console.log(result);
+
